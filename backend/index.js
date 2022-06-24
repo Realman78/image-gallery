d9dbe6493b1ddeb1ceb9fdbc64b5912ca836d2a1
Realman78 <marin.dedic@me.com>
2022-06-24 18:55:17 +0200

    Frontend almost done. TODO: commenting through UI, mobile support

--- backend/index.js
+++ backend/index.js
@@ -10,24 +10,23 @@ const socketServer = require('./socket')
 const cluster = require('node:cluster')
 const numCPUs = require('node:os').cpus().length;
 cluster.schedulingPolicy = cluster.SCHED_RR
-if (cluster.isPrimary) {
-    console.log(`Primary ${process.pid} is running`);
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork()
-    }
-    cluster.on("exit", (worker, code, signal) => {
-        console.log(`worker ${worker.process.pid} died`);
-        cluster.fork();
-    })
-} else {
+// if (cluster.isPrimary) {
+//     console.log(`Primary ${process.pid} is running`);
+//     for (let i = 0; i < numCPUs; i++) {
+//         cluster.fork()
+//     }
+//     cluster.on("exit", (worker, code, signal) => {
+//         console.log(`worker ${worker.process.pid} died`);
+//         cluster.fork();
+//     })
+// } else {
     const app = express()
     console.log(`Worker ${process.pid} started`);
     const server = http.createServer(app)
-    socketServer.registerSocketServer(server)
     app.use(cors())
     app.use(express.json({ limit: '50mb' }));
     app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-
+    
     //API routes
     const authRouter = require('./routes/authRoutes')
     const postRouter = require('./routes/postRoutes')
@@ -35,8 +34,8 @@ if (cluster.isPrimary) {
     app.use('/api/auth', authRouter)
     app.use('/api/posts', postRouter)
     app.use('/api/comments', commentRouter)
-
-    app.listen(PORT, () => {
+    socketServer.registerSocketServer(server)
+    
+    server.listen(PORT, () => {
         console.log(`Server is up and running on port ${PORT}`)
     })
-}
